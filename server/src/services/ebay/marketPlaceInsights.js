5b77cc367a60814b2730275e2436060519c784bd
Douglas Uretsky <douglasuretsky@gmail.com>
2023-10-28 13:21:26 -0400

    add folders

--- server/src/services/ebay/marketPlaceInsights.js
+++ server/src/services/ebay/marketPlaceInsights.js
@@ -5,20 +5,17 @@ import fetchToken from "./authentication/index.js";
 async function fetchMarketplaceInsights(queryParams = {}) {
   const token = await fetchToken();
 
-  const url = `${BASE_URL}/buy/marketplace_insights/v1_beta/item_sales/search?`; // Adjust this endpoint to the specific API call you're making within Marketplace Insights
+  const url = `${BASE_URL}/buy/marketplace_insights/v1_beta/item_sales/search`; // Adjust this endpoint to the specific API call you're making within Marketplace Insights
 
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-    // params: queryParams,
+    params: querystring.stringify(queryParams),
   };
 
   try {
-    const response = await axios.get(
-      `${BASE_URL}/buy/browse/v1/item_summary/search?q=shirt&category_ids=15724&aspect_filter=categoryId%3A15724%2CColor%3A%7BRed%7D`,
-      config
-    );
+    const response = await axios.get(url, config);
     console.log(response.data);
     return response.data;
   } catch (error) {
